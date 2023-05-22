25df1ca476cbdb5b29a93d49bbcbc421cc46ed37
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-05-22 12:18:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -130,3 +130,21 @@
 
 // console.log(findUnique(arr1));
 // console.log(findUnique(arr2));
+
+// ⭐⭐⭐
+// (Ускладнене. Задача не оцінюється. Для тих, кому хочеться поробити ще щось)
+// Створити функцію create() , яка приймає один аргумент у вигляді рядка.
+//Ця функція повертає анонімну функцію, яка перевіряє чи переданий в неї аргумент збігається з аргументом зовнішньої функції.
+// const tom = create("pass_for_Tom");
+// tom("pass_for_Tom"); //повертає true
+// tom("pass_for_tom"); //повертає false
+
+const create = (string) => {
+  return (string1) => {
+    return string === string1 ? true : false;
+  };
+};
+
+const tom = create("pass_for_Tom");
+console.log(tom("pass_for_Tom"));
+console.log(tom("pass_for_tom"));

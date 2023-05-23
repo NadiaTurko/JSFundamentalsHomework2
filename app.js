1028f2371eb7a0334af7f0964107c9c82a34f52d
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-05-23 18:11:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 // 1.
 // Дано три цілих числа: a, b, c. Перевірте істинність висловлювання:
 //  a < b < c
+if (a < b && b < c && a < c) {
+  console.log(true);
+} else {
+  console.log(false);
+}
 // const a = 5;
 // const b = 15;
 // const c = 65;
